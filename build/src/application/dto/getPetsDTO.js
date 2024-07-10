f3834f925d952405cccf1b295b8b5174b3c90a2e
root <root@DESKTOP-FUUHGOR.localdomain>
2024-07-10 14:14:57 +0500

    fix minor issues

---
+++ build/src/application/dto/getPetsDTO.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GetPetsDTO = void 0;
+class GetPetsDTO {
+    constructor(dto) {
+        this.name = dto.name;
+        this.species = dto.species;
+        this.customerId = dto.customerId;
+    }
+    static createDTO(dto) {
+        return new GetPetsDTO(dto);
+    }
+}
+exports.GetPetsDTO = GetPetsDTO;
