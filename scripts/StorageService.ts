6a33f1301161fab9ab13f04302d9925e1d6312cf
root <root@DESKTOP-FUUHGOR.localdomain>
2024-07-12 19:06:44 +0500

    fix minor issues

---
+++ scripts/StorageService.ts
@@ -0,0 +1,27 @@
+import { google } from 'googleapis';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export abstract class StorageService {
+  protected auth: any;
+  protected drive: any;
+
+  constructor() {
+    const keyFile = process.env['GOOGLE_SERVICE_ACCOUNT_KEY'];
+    if (!keyFile) {
+      throw new Error('Missing GOOGLE_SERVICE_ACCOUNT_KEY in environment variables.');
+    }
+
+    this.auth = new google.auth.GoogleAuth({
+      keyFile,
+      scopes: ['https://www.googleapis.com/auth/drive.file'],
+    });
+
+    this.drive = google.drive({ version: 'v3', auth: this.auth });
+  }
+
+  abstract uploadFile(fileName: string, filePath: string, mimeType: string): Promise<void>;
+
+  abstract searchFile(fileName: string, folderId: string): Promise<any[]>;
+}
